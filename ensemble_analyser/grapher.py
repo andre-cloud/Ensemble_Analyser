3d0f5a7ecdef5e5a21bfb48fab3074b8012ba2a4
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2023-05-08 15:58:32 +0200

    Update grapher.py

--- ensemble_analyser/grapher.py
+++ ensemble_analyser/grapher.py
@@ -197,8 +197,8 @@ class Graph:
 
 
         confidence = 0.01
-        initial_guess = [.4, -1, confidence]
-        result = opt.minimize(optimiser, initial_guess, bounds=[(1/3, 0.8), (-2, 2), (0.01, 0.01)])
+        initial_guess = [.3, -1, confidence]
+        result = opt.minimize(optimiser, initial_guess, bounds=[(.2, 0.8), (-2, 2), (0.01, 0.01)])
         if result.success:
             sigma, shift, thr = result.x
             self.log.info(f'Convergence of parameters succeeded within a threshold of {thr:.2f}u.a. for the ∆ε. Confidence level: {(1-result.fun/(2*X[np.where((X>=x_min) & (X<=x_max))].size))*100:.2f}%. Parameters obtained\n\t- σ = {sigma:.4f} eV (that correspond to a FWHM = {(sigma*np.sqrt(2*np.log(2))*2):.4f} eV\n\t- Δ = {shift:.4f} eV (in this case, a negative shift corresponds to a RED-shift)')
