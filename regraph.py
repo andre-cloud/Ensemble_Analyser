b45d3f202b1ca0a66abc820289cdd85c22205f6b
Andrea Pellegrini <55535285+andre-cloud@users.noreply.github.com>
2024-05-06 17:27:11 +0200

    Update regraph.py

--- regraph.py
+++ regraph.py
@@ -18,7 +18,6 @@ conformers, protocol, start_from = restart()
 
 settings = json.load(open("settings.json"))
 output = 'regraph.log'
-cpu = settings.get("cpu", args.cpu)
 temperature = settings.get("temperature", args.temperature)
 final_lambda = settings.get("final_lambda", 800)
 definition = settings.get("definition", 4)
